0ce999688f24a730608afae8d26ad7310921282d
Macbook <macbook@nikolay-macbook.local>
2019-02-17 22:50:24 +0200

    Initial Commit

---
+++ CRM Repair Master/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CRM Repair Master
+//
+//  Created by Macbook on 17.02.2019.
+//  Copyright © 2019 Nikolay_Romanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
